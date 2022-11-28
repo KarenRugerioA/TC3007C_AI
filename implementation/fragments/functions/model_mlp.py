48cf6017f90c38be044143e8c7de062c086fd9f6
myrosandrade <myrosandrade89@gmail.com>
2022-11-27 16:20:12 -0800

    MLP model updated

--- implementation/fragments/functions/model_mlp.py
+++ implementation/fragments/functions/model_mlp.py
@@ -2,25 +2,31 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_val_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import KFold
 from joblib import dump
 from sklearn.model_selection import cross_validate
+import warnings
 
 
 # Function that trains the model mlp
-def train_mlp(target_column_name, original_name_dataset):
+def train_mlp(target_column_name, original_name_dataset, smote):
+
+    warnings.filterwarnings("ignore")
 
     # Obtaining the train and test dataset
-    train = pd.read_csv(f'../data/{original_name_dataset}/train/original_train.csv')
     x_test = pd.read_csv(f'../data/{original_name_dataset}/test/x_test.csv')
-    y_test = pd.read_csv(f'../data/{original_name_dataset}/test/y_test.csv')
+    y_test = pd.read_csv(f'../data/{original_name_dataset}/test/y_test.csv')    
 
-    # Dividing the train dataset
-    x_train = train.drop([target_column_name], axis=1)
-    y_train = pd.DataFrame(train[target_column_name])
+    if smote:
+        x_train = pd.read_csv(f'../data/{original_name_dataset}/train/x_train.csv')
+        y_train = pd.read_csv(f'../data/{original_name_dataset}/train/y_train.csv')
+    else:    
+        # Dividing the train dataset
+        train = pd.read_csv(f'../data/{original_name_dataset}/train/original_train.csv')
+        x_train = train.drop([target_column_name], axis=1)
+        y_train = pd.DataFrame(train[target_column_name])
 
     #Initializing the MLPClassifier hyperparameters
     classifier = MLPClassifier(alpha=1e-05, hidden_layer_sizes=(5, 2), random_state=1, solver='lbfgs')
@@ -41,18 +47,15 @@ def train_mlp(target_column_name, original_name_dataset):
     #Evaluataion of the predictions against the actual observations in y_val
     cm = confusion_matrix(y_pred, y_test)
 
-    #Printing the accuracy
+    #Storing the accuracy
     acc = round(accuracy(cm),2)
     percentage = "{:.0%}".format(acc)
     model_accuracy = (f"Accuracy of Model: {percentage}")
-    print(model_accuracy)
 
     # Confussion Matrix
     confussion_matrix = pd.DataFrame(cm)
-    print(confussion_matrix)
 
     # K-Fold Cross-Validation
-    kf = KFold(n_splits=3)
     def cross_validation(model, _X, _y, _cv=3):
         _scoring = ['accuracy', 'precision', 'recall', 'f1']
         results = cross_validate(estimator=model,
@@ -96,12 +99,13 @@ def train_mlp(target_column_name, original_name_dataset):
         plt.ylabel(y_label, fontsize=14)
         plt.legend()
         plt.grid(True)
-        plt.savefig(f'./fragments/joblibs/{original_name_dataset}/model/k_cross_plot.png')
+        plt.savefig(f'./fragments/joblibs/{original_name_dataset}/model/mlp/k_cross_plot.png')
         plt.show()
 
     mlp_results = cross_validation(classifier, x_train, y_train)
 
     # Plot Accuracy Result
+    print('Printing the K-fold Cross Validation')
     model_name = "MLP"
     plot_result(model_name,
                 "Accuracy",
@@ -109,7 +113,11 @@ def train_mlp(target_column_name, original_name_dataset):
                 mlp_results["Training Accuracy scores"],
                 mlp_results["Validation Accuracy scores"])
 
-    dump(classifier, f"./fragments/joblibs/{original_name_dataset}/model/classification-model.joblib")
+    # Storing the model
+    if smote:
+        dump(classifier, f"./fragments/joblibs/{original_name_dataset}/model/mlp/mlp_model_smote.joblib")
+    else:    
+        dump(classifier, f"./fragments/joblibs/{original_name_dataset}/model/mlp/mlp_model.joblib")
 
     # Returning the results of the training model
     return model_accuracy, confussion_matrix, mlp_results
